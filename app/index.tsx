4bbdec4341c1e179c39d2f7f3ecc9105b444df47
Vishnu Prasad Kuntar <kuntarvishnuprasad41@gmail.com>
2025-06-30 07:25:48 +0530

    clear

--- app/index.tsx
+++
@@ -1,9 +0,0 @@
-import { Text, View } from "react-native";
-
-export default function Index() {
-  return (
-    <View className="flex-1 bg-black h-screen w-screen items-center justify-center">
-      <Text>Edit app/index.tsx to edit this screen.</Text>
-    </View>
-  );
-}
